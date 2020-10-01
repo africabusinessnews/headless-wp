2fb0517d500fa45187b1bee17908e73815e6604b
sidwahi <git@cma-ih.com>
2020-10-01 13:19:11 +0400

    Brightcove added

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,13 +1,15 @@
 import axios from "axios"
 let dynamicRoutes = () => {
   const routes = axios
-    .get("https://www.cnbcafrica.com/wp-json/wp/v2/posts?page=1&per_page=20")
+    .get("https://wwww.cnbcafrica.com/wp-json/wp/v2/posts?page=1&per_page=20")
     .then(res => {
       return res.data.map(post => `/blog/${post.slug}`)
     })
   console.log(routes)
   return routes
 }
+
+
 
 export default {
   mode: "universal",
